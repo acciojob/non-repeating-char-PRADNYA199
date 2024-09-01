32cc60d81752671c30efbe6694d663c424b50801
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-09-01 08:55:40 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ // Write your code herele
+	const charCount={};
+	for ( let char of str){
+		if(charCount[char])
+		{
+			charCount++;
+		}
+		else{
+			charCount=1;
+		}
+	}
+	for(let char of str)
+		{
+			if(charCount[char]===1)
+			{
+				return char;
+			}
+		}
+	return null;
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
